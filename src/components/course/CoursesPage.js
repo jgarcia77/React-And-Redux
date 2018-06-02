150c1d776d5ffad8105c2dc538cf60912fc04366
Josue Garcia <JosueG@Magenic.com>
2018-06-02 12:50:36 -0500

    Async in Redux

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,49 +2,18 @@ import React, {PropTypes} from 'react';
 import { connect } from 'react-redux';
 import * as courseActions from '../../actions/courseActions';
 import { bindActionCreators } from 'redux';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
-
-    this.state = {
-      course: {
-        title: ''
-      }
-    };
-
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
-  }
-
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course: course});
-  }
-
-  onClickSave(event) {
-    this.props.actions.createCourse(this.state.course);
-  }
-
-  courseRow(course, index) {
-    return <div key={index}>{course.title}</div>;
   }
 
   render() {
     return (
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
-        <h2>Add Course</h2>
-
-        <input type="text"
-          value={this.state.course.title}
-          onChange={this.onTitleChange} />
-
-        <input type="submit"
-          value="Save"
-          onClick={this.onClickSave} />
+        <CourseList courses={this.props.courses} />
       </div>
     );
   }
